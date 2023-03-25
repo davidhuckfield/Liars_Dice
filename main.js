106cb381bd7e96820bbf27d5460f1670334e7ace
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-03-25 10:07:56 +0000

    Added text input and button to enter name, identified these in JS, added function to change the welcome message with the user's name

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log(`Javascript has been linked!`);
+let namebutton=document.querySelector(`#name-button`);
+let namebox=document.querySelector(`#name-box`);
+let welcomemessage=document.querySelector(`#welcome-message`);
+
+namebutton.addEventListener(`click`, enterName);
+
+function enterName(){
+    welcomemessage.textContent=`Welcome to Liar's Dice, ${namebox.value}!`;
+}
