c44bb9bb14d29a4537b23f59a53435fda6418375
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-03-25 10:50:09 +0000

    Added player dice sections in html, added random dice roll function in JS to generate 5 random numbers between 1 & 6 and enter them into the html text content

--- main.js
+++ main.js
@@ -2,8 +2,25 @@ console.log(`Javascript has been linked!`);
 let namebutton=document.querySelector(`#name-button`);
 let namebox=document.querySelector(`#name-box`);
 let welcomemessage=document.querySelector(`#welcome-message`);
+let rolldicebutton=document.querySelector(`#rolldice`);
+const playerdicelocations = [];
+for (let i = 1; i <= 5; i++) {
+  playerdicelocations[i-1] = document.querySelector(`#playerdice${i}`);
+}
+console.log(playerdicelocations);
+const playerroll =[];
 
 namebutton.addEventListener(`click`, enterName);
+rolldicebutton.addEventListener(`click`, rollDice)
+
+function rollDice (){
+    for (let i=0; i<=4; i++) {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    playerroll[i]=randomNumber;
+    playerdicelocations[i].textContent=playerroll[i];
+    }   
+
+}
 
 function enterName(){
     welcomemessage.textContent=`Welcome to Liar's Dice, ${namebox.value}!`;
